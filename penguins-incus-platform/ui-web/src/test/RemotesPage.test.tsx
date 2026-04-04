9aada85ce9ffba45117dcabbb141ec3deddf2614
Interested Deving <interested.deving.1896@gmail.com>
2026-04-04 04:20:32 +0000

    fix(test): remove unused within import from RemotesPage.test.tsx

--- penguins-incus-platform/ui-web/src/test/RemotesPage.test.tsx
+++ penguins-incus-platform/ui-web/src/test/RemotesPage.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent, waitFor, within } from "@testing-library/react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import { RemotesPage } from "../pages/RemotesPage";
 
 const REMOTES = [
